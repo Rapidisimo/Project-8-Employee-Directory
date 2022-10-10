a583a099a19daf0702c732ca8fc4affff88ac481
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2022-10-10 11:01:13 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+fetch('https://randomuser.me/api/?inc=picture,name,email,location,&results=12')
+  .then(response => response.json())
+  .then(data => console.log(data.results))
+// Data to get   
+    // Image
+    // First and Last Name
+    // Email
+    // City
+
+
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+
+
+
+// ------------------------------------------
+//  POST DATA
+// ------------------------------------------
+
