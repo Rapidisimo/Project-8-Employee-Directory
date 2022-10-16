59ee0822321acb3066b379b3c326d9c8228dc63b
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2022-10-15 20:23:23 -0400

    working version

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-let employeeList = [];
-const apiURL =`https://randomuser.me/api/?inc=picture,name,email,location,&results=12`;
-const gridElement = document.querySelector("main");
+let employees = [];
+const urlAPI =`https://randomuser.me/api/?results=12&inc=name, picture,email, location, phone, dob &noinfo &nat=US`;
+const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal");
+const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch(apiURL)
+fetch(urlAPI)
   .then(response => response.json())
   .then(res => res.results)
   .then(displayEmployees)
@@ -22,11 +22,11 @@ fetch(apiURL)
 // ------------------------------------------
 function displayEmployees (employeeData) {
     // To access outside of this function
-    employeeList = employeeData;
+    employees = employeeData;
     // store the HTML as it's created
     let employeeHTML = '';
     // loop through each emplyee and create html
-    employeeList.forEach( (employee, index) => {
+    employees.forEach( (employee, index) => {
         let name = employee.name;
         let email = employee.email;
         let city = employee.location.city;
@@ -43,15 +43,48 @@ function displayEmployees (employeeData) {
             </div>
             `    
     })
-    gridElement.innerHTML = employeeHTML;
+    gridContainer.innerHTML = employeeHTML;
 };
 
+function displayModal(index) {
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+    <img src="${picture.large}" alt="profile-photo" class="profile-photo">
+    <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address">${street}, ${state} ${postcode}</p>
+        <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    `;
+    
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+    console.log('displayModal-index')
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
+gridContainer.addEventListener('click', e => {
+    if (e.target !== gridContainer) {
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        displayModal(index);
+        console.log('gridContainer-Click');
+    }
+});
 
-
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
 
 // ------------------------------------------
 //  POST DATA
